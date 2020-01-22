48e062abcc1228185944d457b19a3ec65eba5ddc
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-01-23 02:49:12 +0800

    refactor: clean code, change print format of product indices

--- linear_algebra/matrix_multiplication.py
+++ linear_algebra/matrix_multiplication.py
@@ -5,50 +5,30 @@ from __future__ import print_function
 __version__: '1.0.2'
 __author__: 'Jan Rodolf Espinas'
 
-def create_matrix():
-    rows = int(input('Number of rows: '))
-    columns = int(input('Number of columns: '))
+# import local file containing matrix operations
+import matrix_operations
 
-    input_matrix = []
-
-    for row in range(0,rows):
-        input_matrix.append([int(input(f'M[{row+1},{col+1}] = ')) \
-        for col in range(columns)])  
-
-    return input_matrix
-
-def transpose_matrix(matrix):
-    rows = len(matrix)
-    columns = len(matrix[0])
-
-    transposed_matrix = []
-    
-    for col in range(0, columns):
-        transposed_matrix.append([matrix[i][col] for i in range(rows)])
-
-    return transposed_matrix
-
-def display_matrix(matrix):
-    print('\n')
-    [print(row) for row in matrix]
-
-def matrix_multiplication(matrix_one, matrix_two):
+def matrix_mul(matrix_one, matrix_two):
     if len(matrix_one[0]) == len(matrix_two):
         product_matrix = []
         product_matrix_rows = len(matrix_one)
         product_matrix_columns = len(matrix_two[0])
         
-        matrix_two = transpose_matrix(matrix_two)
+        matrix_two = matrix_operations.transpose(matrix_two)
         
-        for row in range(0, product_matrix_rows):
-            for column in range(0, product_matrix_columns):
-                print(row,column)
+        # prints indices
+        for row in range(product_matrix_rows):
+            product_matrix.append([(row,column) for column in range(product_matrix_columns)])
 
     else:
-        print('The matrix can not be multiplied.\n Be sure to input the right dimensions of both matrix.')
+        print('The matrix can not be multiplied.\n \
+         Be sure to input the right dimensions of both matrix.')
 
     return product_matrix
+
 if __name__ == '__main__':
-    A = create_matrix()
-    B = create_matrix()
-    matrix_multiplication(A,B)
+    A = matrix_operations.create_matrix()
+    B = matrix_operations.create_matrix()
+    C = matrix_mul(A,B)
+    matrix_operations.display(C)
+
