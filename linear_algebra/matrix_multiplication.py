7daac71732f3d423b87200ce9f2376c9da5e8c02
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-01-22 23:48:06 +0800

    feat: print the indices of the product of two matrices

--- linear_algebra/matrix_multiplication.py
+++ linear_algebra/matrix_multiplication.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__: '1.0.1'
+__version__: '1.0.2'
 __author__: 'Jan Rodolf Espinas'
 
 def create_matrix():
@@ -34,11 +34,20 @@ def display_matrix(matrix):
 
 def matrix_multiplication(matrix_one, matrix_two):
     if len(matrix_one[0]) == len(matrix_two):
-        print('It can be multiplied')
-        transpose_matrix(matrix_two)
+        product_matrix = []
+        product_matrix_rows = len(matrix_one)
+        product_matrix_columns = len(matrix_two[0])
+        
+        matrix_two = transpose_matrix(matrix_two)
+        
+        for row in range(0, product_matrix_rows):
+            for column in range(0, product_matrix_columns):
+                print(row,column)
+
     else:
         print('The matrix can not be multiplied.\n Be sure to input the right dimensions of both matrix.')
 
+    return product_matrix
 if __name__ == '__main__':
     A = create_matrix()
     B = create_matrix()
