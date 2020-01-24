963da060f303f2084c81f2abe38f6f7c3c3a1f84
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-01-25 02:25:01 +0800

    feat: can now perform outer product

--- linear_algebra/vector_multiplication.py
+++ linear_algebra/vector_multiplication.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__: '1.0.1'
+__version__: '1.2.0'
 __author__: 'Jan Rodolf Espinas'
 
 import matrix_operations # My linear algebra library
@@ -15,7 +15,7 @@ def vector_multiplication(matrix_one, matrix_two):
     matrix_two_columns = len(matrix_two[0])
 
     # inner product
-    if matrix_one_columns == matrix_two_rows \
+    if (matrix_one_columns == matrix_two_rows)\
     and (matrix_one_rows == 1 and matrix_two_columns == 1):
 
         dot_product = []
@@ -31,10 +31,17 @@ def vector_multiplication(matrix_one, matrix_two):
         return dot_product
 
     # outer product
-    elif matrix_one_rows == matrix_two_columns \
-    and (matrix_one_columns == 1 and matrix_two_rows == 1):
+    elif matrix_one_columns == 1 and matrix_two_rows == 1:
     
-        print("outer")
+        outer_product = []
+
+        for row in range(matrix_one_rows):
+            outer_product.append([
+                matrix_one[row][0] * matrix_two[0][col]
+                for col in range(matrix_two_columns)
+            ])
+
+        return outer_product
     
     else:
         print("dimensions of vector do not match.")
@@ -42,7 +49,7 @@ def vector_multiplication(matrix_one, matrix_two):
 if __name__ == '__main__':
     a = matrix_operations.create_matrix()
     b = matrix_operations.create_matrix()
-    a = matrix_operations.transpose(a)
+    b = matrix_operations.transpose(b)
     c = vector_multiplication(a,b)
     matrix_operations.display(c)
     
