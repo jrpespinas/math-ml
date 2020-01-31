d709b096921ca8da29bb6ddc988a8565b5ec4ae5
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-02-01 00:44:32 +0800

    feat: added a function to get delta x

---
+++ numerical_analysis/riemann_sum.py
@@ -0,0 +1,15 @@
+"""Riemann Sums: Intro to Numerical Analysis"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__version__: '1.0.1'
+__author__: 'Jan Rodolf Espinas'
+
+def delta_x(lower_bound, upper_bound, n):
+    return (lower_bound - upper_bound)/n
+
+def riemann_sum():
+
+if __name__ == '__main__':
