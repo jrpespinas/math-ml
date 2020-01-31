10499225b7205f86c8f735bf196a7f34fbbdb67d
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-02-01 01:38:12 +0800

    feat: added a function that returns a value of f(x)

--- numerical_analysis/riemann_sum.py
+++ numerical_analysis/riemann_sum.py
@@ -7,9 +7,15 @@ from __future__ import print_function
 __version__: '1.0.1'
 __author__: 'Jan Rodolf Espinas'
 
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns  
+
 def delta_x(lower_bound, upper_bound, n):
     return (lower_bound - upper_bound)/n
 
-def riemann_sum():
+def f(x):
+    return (2*(x**3)) + (x**2) - x + 1
 
 if __name__ == '__main__':
+    
