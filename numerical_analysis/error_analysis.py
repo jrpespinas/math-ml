bd1897acc21b2a1a86797df1e149dd932ad34bd2
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-02-02 01:54:47 +0800

    refactor: Use abs() from py instead of self-implementation

--- numerical_analysis/error_analysis.py
+++ numerical_analysis/error_analysis.py
@@ -7,16 +7,11 @@ from __future__ import print_function
 __version__: '1.0.1'
 __author__: 'Jan Rodolf Espinas'
 
-def absolute_value(value):
-    if value <= 0:
-        return value * -1
-    return value
-
 def absolute_error(true_fx, approximate_fx):
-    return absolute_value(true_fx - approximate_fx)
+    return abs(true_fx - approximate_fx)
 
 def relative_error(true_fx, approximate_fx):
-    return absolute_error(true_fx, approximate_fx)/absolute_value(true_fx)
+    return absolute_error(true_fx, approximate_fx)/abs(true_fx)
 
 if __name__ == '__main__':
     print(relative_error(10,9))
