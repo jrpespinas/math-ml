8c6461214dffae3e786e99ebc59517f9be4f1292
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-02-11 22:16:30 +0800

    chore: removed strings from functions

--- numerical_analysis/riemann_sum.py
+++ numerical_analysis/riemann_sum.py
@@ -16,16 +16,16 @@ def get_delta_x(lower_bound, upper_bound, n):
     return (upper_bound-lower_bound) / n
 
 def f1(x):
-    return (2*(x**3)) + (x**2) - x + 1, "2x^3+x^2-x+1"
+    return (2*(x**3)) + (x**2) - x + 1
 
 def f2(x):
-    return (2*(x**2)) - x + 1, "2x^2-x+1"
+    return (2*(x**2)) - x + 1
 
 def f3(x):
-    return np.sin(x), "sin(x)"
+    return np.sin(x)
 
 def f4(x):
-    return x**2, "x^2"
+    return x**2
 
 def graph_function(function,x,n):
     """
@@ -48,10 +48,10 @@ def graph_function(function,x,n):
        The integral sum
     """
     riemann_sum = get_riemann_sum(function,x,n)
-    y, fx = function(x) 
+    y = function(x) 
     
     plt.plot(x, y, label = f'Riemann Sum: {riemann_sum}')
-    plt.title(fr'Graph of $y = {fx}$')
+    plt.title(fr'Graph of $y = f(x)$')
     plt.legend()
     plt.grid()
 
@@ -77,8 +77,7 @@ def get_riemann_sum(function, x, delta_x):
     float
        The integral sum
     """
-    fx, string = function(x)
-    return sum(fx*delta_x)
+    return sum(function(x)*delta_x)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Riemann Integrals")
